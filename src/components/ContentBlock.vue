0c8f5cb1c9ea5c3dc2c8b23b4f68a1fd7304dd4e
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-11-02 11:20:52 +0500

    Add request imitation for users

--- src/components/ContentBlock.vue
+++ src/components/ContentBlock.vue
@@ -75,54 +75,8 @@ import {useStore} from 'vuex';
 import {Users, UsersHeader, UsersDivider} from '@/interfaces/users';
 import {User} from '@/interfaces/user';
 import {isCountryEqual, isScoreRight} from '@/utils/filters';
+import {fetchUsers as fetchUsersFromServer} from '@/services/service';
 
-const USERS = [
-    {header: 'List'},
-    {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        title: 'Brunch this weekend?',
-        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        country: 'usa',
-        score: 12,
-        place: 'Washington',
-    },
-    {divider: true, inset: true},
-    {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        country: 'russia',
-        score: 1,
-        place: 'Perm',
-    },
-    {divider: true, inset: true},
-    {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-        title: 'Oui oui',
-        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        country: 'usa',
-        score: 21,
-        place: 'New York',
-    },
-    {divider: true, inset: true},
-    {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-        title: 'Birthday gift',
-        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        country: 'russia',
-        score: 31,
-        place: 'Moscow',
-    },
-    {divider: true, inset: true},
-    {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-        title: 'Recipe to try',
-        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        country: 'russia',
-        score: 101,
-        place: 'Vologda',
-    },
-];
 const store = useStore();
 
 const users = ref<Users>([]);
@@ -143,12 +97,8 @@ const countryVariant = computed({
     set: (val: string | null) => store.commit('setCountryFilterVariant', val)
 });
 
-async function fetchUsers() {
-    const pr = new Promise(resolve => {
-        setTimeout(() => resolve(true), 1000 * 1);
-    });
-    await pr;
-    users.value = USERS;
+async function fetchUsers(): Promise<void> {
+    users.value = await fetchUsersFromServer();
 }
 
 onBeforeMount(() => {
